a5b57c3503b303f2e7d98563ee1a4174d281895d
David Schwelien <davidschwelienalt@davids-air.srg-event.ch>
2017-02-02 10:08:09 +0100

    test

--- API-Read.R
+++ API-Read.R
@@ -1,45 +1,63 @@
-### Read APIs using R 
+#---- initial and functions ---#  
 
+# Literatur: https://www.r-bloggers.com/accessing-apis-from-r-and-a-little-r-programming/
 
+options(stringsAsFactors = FALSE) #just do that...  
 
-install.packages(c("httr", "jsonlite", "lubridate"))
-library(httr)
-library(jsonlite)
-## 
-## Attaching package: 'jsonlite'
-## 
-## The following object is masked from 'package:utils':
-## 
-##     View
-library(lubridate)
-options(stringsAsFactors = FALSE)
+packages = c("httr", "jsonlite", "lubridate") #packete laden: 
+for (package in packages) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, repos="http://cran.rstudio.com/")
+    library(package, character.only = TRUE)
+  }
+}
+# function zum abrufen der API und schreiben in DF
+GETtoDataFrame <- function(cURL) {
+  raw.result <- GET(cURL)
+  this.raw.content <- rawToChar(raw.result$content)
+  this.content <- fromJSON(this.raw.content)
+  this.content.df <- do.call(what = "rbind",
+                             args = lapply(this.content, as.data.frame))
+}
 
-# so wie hier: https://www.r-bloggers.com/accessing-apis-from-r-and-a-little-r-programming/
-url  <- "http://srgssr-prod.apigee.net/rts-archives-public-api/archives"
-path <- "/rts-archives-public-api/archives?apikey=O7lSaGGuhqBcLNB4yPFmnZiT26bfFN4r "
-raw.result <- GET(url = url, path = path)
-
-# oder GET aus der cURL...
+# Anwendung: 
+# Aus API App: 
 cURL <- "http://srgssr-prod.apigee.net/rts-archives-public-api/archives?apikey=O7lSaGGuhqBcLNB4yPFmnZiT26bfFN4r"
+api_out <- GETtoDataFrame(cURL = cURL)
+api_out[1,]
 
-raw.result <- GET(cURL)
-this.raw.content <- rawToChar(raw.result$content)
-nchar(this.raw.content)
-substr(this.raw.content, 1, 100)
+# oder so: 
 
-this.content <- fromJSON(this.raw.content)
-this.content[[1]]
-raw.result$content
-names(raw.result)
-raw.result$status_code
-raw.result$request
-raw.result$cookies
+endpoint <- "http://srgssr-prod.apigee.net/integrationlayer/2.0/srf/showList/tv/alphabetical"
+apikey <- "O7lSaGGuhqBcLNB4yPFmnZiT26bfFN4r"
+characterFilter <- "a" 
+cURL <- paste0(endpoint , "?apikey=", apikey, "&characterFilter=", characterFilter )
+api_out <- GETtoDataFrame(cURL = cURL)
+api_out[1,]
 
-# Get list of all channels
-cURL <- "http://services.sg1.dev02.etvt01.sctv.ch/catalog/tv/channels/list/(ids=all;sa=true)"
-raw.result <- GET(cURL)
-this.raw.content <- rawToChar(raw.result$content)
-substr(this.raw.content, 1, 100)
 
-this.content <- fromJSON(this.raw.content)
-this.content[[1]]
+#### most watched für Basil
+cURL <-"http://srgssr-prod.apigee.net/integrationlayer/2.0/srf/mediaList/video/mostClicked?apikey=O7lSaGGuhqBcLNB4yPFmnZiT26bfFN4r"
+
+api_out <- GETtoDataFrame(cURL = cURL)
+api_out[1,]
+
+
+
+#### teletext <- hier gibt es die Infos für die CSV für Basil. noch rausfinden welche Seite.... 
+cURL <-  "http://srgssr-prod.apigee.net/teletext/live/page?page=201&channel=SRF1&apikey=O7lSaGGuhqBcLNB4yPFmnZiT26bfFN4r"
+#endpoint <- "http://srgssr-prod.apigee.net/teletext/live/page"
+#apikey <- "O7lSaGGuhqBcLNB4yPFmnZiT26bfFN4r"
+#cURL <- paste0(endpoint , "?apikey=", apikey )
+api_out <- GETtoDataFrame(cURL = cURL)
+api_out[1,] #Encoding ??? WTF...
+
+
+#### landingpage-landingPageContentId-get <- was ist die "landingPageContentId" 
+#### https://api.srgssr.ch/audio-and-video-metadata/apis
+
+##### ∂€v 
+
+
+names(api_out)
+head(api_out$numFound)
